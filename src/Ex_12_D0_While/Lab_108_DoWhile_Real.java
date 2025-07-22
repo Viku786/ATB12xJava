af613776ccd874d6eea37a9ab89f4ee16a2621e5
Viku786 <kkvikash2016@gmail.com>
2025-07-22 09:06:11 +0530

    18th may

---
+++ src/Ex_12_D0_While/Lab_108_DoWhile_Real.java
@@ -0,0 +1,16 @@
+package Ex_12_D0_While;
+
+public class Lab_108_DoWhile_Real {
+    public static void main(String[] args) {
+
+        //Web Automation
+        int number =0;
+
+        do {
+            System.out.println(number);
+            //call Function (Number)
+            //Close the Excel file and open the file
+            number++;
+        }while (number < 0);
+    }
+}
