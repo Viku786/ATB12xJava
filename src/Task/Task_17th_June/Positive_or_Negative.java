8a68106c6ccc84eec710800855d8e37bd2e76cd8
Viku786 <kkvikash2016@gmail.com>
2025-07-30 13:03:24 +0530

    17 June

---
+++ src/Task/Task_17th_June/Positive_or_Negative.java
@@ -0,0 +1,22 @@
+package Task.Task_17th_June;
+
+import java.util.Scanner;
+
+public class Positive_or_Negative {
+    public static void main(String[] args) {
+
+        //1) Check if a Number is Positive or Negative.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        if(num == 0){
+            System.out.println("The enter number " +num+ " neither positive nor negative number");
+        } else if (num > 0) {
+            System.out.println("The enter number " +num+ " is Positive Number");
+        }
+        else {
+            System.out.println("The enter number " +num+ " is Negative Number");
+        }
+    }
+}
---
+++ src/Task/Task_17th_June/Vowel_or_Consonant.java
@@ -0,0 +1,22 @@
+package Task.Task_17th_June;
+
+import java.util.Scanner;
+
+public class Vowel_or_Consonant {
+    public static void main(String[] args) {
+
+        //4) Check if a Character is a Vowel or Consonant.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Character: ");
+        char ch = sc.next().toLowerCase().charAt(0);
+
+        if (!Character.isLetter(ch)) {
+            System.out.println("Invalid input! Please enter a letter.");
+        } else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch + " is a vowel.");
+        } else {
+            System.out.println(ch + " is a consonant.");
+        }
+    }
+}
