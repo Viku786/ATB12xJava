cd51058773cbb20676ac154349e9b5e3a5ce0070
Viku786 <kkvikash2016@gmail.com>
2025-08-02 21:02:20 +0530

    5 July test

--- src/Live_Test/String_Assisment/Wrapper_Exception/Qno_5.java
+++ src/Live_Test/String_Assisment/Wrapper_Exception/Qno_5.java
@@ -6,14 +6,12 @@ public class Qno_5 {
     public static void main(String[] args) {
 
 
-        Scanner sc = new Scanner(System.in);// Autoboxing
+        Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        Integer obj = num;
-        System.out.println("Autoboxed: " + obj);
 
+        Integer boxed = num;
+        int unboxed = boxed;
 
-        Integer obj2 = 20;
-        int num2 = obj2;
-        System.out.println("Unboxed: " + num2);
+        System.out.println("Autoboxed: " + boxed + ", Unboxed: " + unboxed);
     }
 }
