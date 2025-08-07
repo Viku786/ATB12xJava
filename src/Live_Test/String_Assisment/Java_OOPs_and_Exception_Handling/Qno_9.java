6106067f79af11cf411babcb85233d787e7a78c5
Viku786 <kkvikash2016@gmail.com>
2025-08-07 12:12:26 +0530

    Test

---
+++ src/Live_Test/String_Assisment/Java_OOPs_and_Exception_Handling/Qno_9.java
@@ -0,0 +1,36 @@
+package Live_Test.String_Assisment.Java_OOPs_and_Exception_Handling;
+
+public class Qno_9 {
+    public static void main(String[] args) {
+
+        /* Create an interface "Drawable" with a method "draw()".
+        Implement this interface in classes "Circle" and "Rectangle".
+         */
+
+        Circle1 c1 = new Circle1();
+        c1.draw();
+
+        Rectangle1 r1 = new Rectangle1();
+        r1.draw();
+    }
+}
+
+interface Drawable{
+    default void draw(){
+        System.out.println("Drawing the different shape");
+    }
+}
+
+class Circle1 implements Drawable{
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Rectangle1 implements  Drawable{
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
